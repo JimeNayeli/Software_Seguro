723eda699f26e0744409e0e10c1130f50c69b740
Jime Tutillo <93277934+JimeNayeli@users.noreply.github.com>
2024-06-04 07:42:36 -0500

    mensaje personalizado flask

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World from Railway with Flask!'
+    return "Hello World, my name is Jimena Tutillo and my classmate Carlos Romero"
+    #return 'Hello World from Railway with Flask!'
 
 if __name__ == '__main__':
     app.run(debug=True)
