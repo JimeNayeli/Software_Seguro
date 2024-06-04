d9f8aefe4416f47e8e9b9193bf1adefc1603ce25
Jime Tutillo <93277934+JimeNayeli@users.noreply.github.com>
2024-06-03 22:07:23 -0500

    New Changes

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello World from Railway with Flask!'
+def index():
+    return jsonify({"Hello World from Railway with Flask"})
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
