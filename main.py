9e403312b72d68dd0bdf35597ccc20948cc694a4
Jime Tutillo <93277934+JimeNayeli@users.noreply.github.com>
2024-06-04 07:36:03 -0500

    mensaje flask

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hola Mundo!'
+    return 'Hello World from Railway with Flask!'
 
 if __name__ == '__main__':
     app.run(debug=True)
